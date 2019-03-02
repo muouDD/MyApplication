3b30fc875942e2d82217b7d00eaec4ef9e6a8b33
dyj <414205373@qq.com>
2019-03-02 16:42:18 +0800

    init

---
+++ app/src/main/java/dyj/com/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package dyj.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "1111111111  ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                Log.e(TAG, "Observable thread is : " + Thread.currentThread().getName());
+                e.onNext(1);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "After observeOn(mainThread)，Current thread is "
+                                + Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .subscribe(new Consumer<Integer>() {
+
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "After observeOn(io)，Current thread is "
+                                + Thread.currentThread().getName());
+                    }
+                });
+
+    }
+}
